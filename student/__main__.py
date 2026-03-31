332a589631a1e4299fc89738f7759a1c75a8bde9
Kylian Mathurin <kmathuri@c1r3p9.42nice.fr>
2026-03-31 15:20:10 +0200

    Feature: Unit answer. Note: The result has to be parsed

--- student/__main__.py
+++ student/__main__.py
@@ -1,19 +1,25 @@
+import time
 from typing import Optional
 import fire
+import re
 import bm25s
 import os
+from transformers import GenerationConfig, pipeline
 import json
 from langchain_text_splitters import (
     Language,
     MarkdownTextSplitter,
     RecursiveCharacterTextSplitter,
 )
+from pydantic_core import from_json
 
 from student.validator import MinimalSource
 
 
 class CLI:
-    def index(self, path: str = "vllm-0.10.1/", max_chunk_size: int = 2000):
+    def index(
+        self, path: str = "vllm-0.10.1/", max_chunk_size: int = 2000
+    ) -> None:
         src = []
         python_content = []
         python_metadata = []
@@ -79,14 +85,53 @@ class CLI:
         with open("data/processed/chunks/corpus.json", "w") as f:
             json.dump(chunk_data, f)
 
-    def search(self):
-        pass
+    def search(self, query: str, k: int):
+        ret_loaded = bm25s.BM25.load(
+            "data/processed/bm25_index", load_corpus=True
+        )
+        docs, scores = ret_loaded.retrieve(bm25s.tokenize(query), k=k)
+        return docs
 
     def search_dataset(self):
         pass
 
-    def answer(self):
-        pass
+    def answer(self, prompt: str, k: int = 2):
+        docs = self.search(prompt, k)
+        context = (
+            "You are a technical assistant specializing in the vLLM codebase. Your goal is to provide accurate answers based ONLY on the provided context.\n"
+            "### MANDATORY RULES ###\n"
+            '1. GROUNDING: Use only the information from the "RETRIEVED_CONTEXT" section. If the answer is not present, state: "I\'m sorry, but the provided context does not contain enough information to answer this question."\n'
+            "2. CITATIONS: You MUST cite the source file path for every claim or code snippet you provide. Use brackets at the end of the sentence, e.g., [vllm/engine/llm_engine.py].\n"
+            "3. BREVITY: Keep your technical explanations concise and focus on the code implementation.)\n"
+            "### RETRIEVED_CONTEXT ###\n"
+        )
+        for doc in docs[0]:
+            context += f"# SOURCE: {doc['file_path']}\n"
+            context += "---\n"
+            context += doc["page_content"]
+            context += "---\n"
+
+        pipe = pipeline("text-generation", model="Qwen/Qwen3-0.6B")
+        message = [
+            {"role": "system", "content": context},
+            {"role": "user", "content": "/no_think " + prompt},
+        ]
+        gen_cfg = GenerationConfig(
+            max_new_tokens=50,
+            do_sample=True,
+        )
+        start = time.time()
+        print(
+            re.sub(
+                r"<think>.*?</think>",
+                "",
+                pipe(message, generation_config=gen_cfg)[0]["generated_text"][
+                    2
+                ]["content"],
+                flags=re.DOTALL,
+            ).strip(".")
+        )
+        print("Time:", time.time() - start)
 
     def answer_dataset(self):
         pass
