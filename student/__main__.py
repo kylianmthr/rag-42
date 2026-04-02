7ff6e7470e54424ed259f6c9a58601ca36a93e8d
Kylian Mathurin <kmathuri@c1r3p9.42nice.fr>
2026-04-02 11:03:48 +0200

    Refactor: Create RAG class with all pipeline logic and move generation logic to the appropriate class

--- student/__main__.py
+++ student/__main__.py
@@ -1,125 +1,48 @@
-import json
-import os
-import time
 import fire
-import re
-import bm25s
-from numpy._typing import NDArray
-from student.indexer import Indexer
+from student.rag import RAG
 from student.validator import (
-    MinimalAnswer,
     MinimalSearchResults,
     MinimalSource,
-    RagDataset,
     StudentSearchResults,
-    StudentSearchResultsAndAnswer,
-    UnansweredQuestion,
 )
-from transformers import (
-    AutoTokenizer,
-    AutoModelForCausalLM,
-)
-from langchain_text_splitters import (
-    Language,
-    MarkdownTextSplitter,
-    RecursiveCharacterTextSplitter,
-)
-
-from student.answer import Answer
 
 
 class CLI:
+    def __init__(self) -> None:
+        self.rag = RAG()
+
     def index(
         self, path: str = "vllm-0.10.1/", max_chunk_size: int = 2000
     ) -> None:
-        indexer = Indexer(path, max_chunk_size)
-        indexer.load_files()
-        indexer.split()
-        indexer.save()
+        self.rag.index(path, max_chunk_size)
 
-    def search(self, query: str, k: int) -> NDArray:
-        ret_loaded = bm25s.BM25.load(
-            "data/processed/bm25_index", load_corpus=True
+    def search(self, query: str, k: int) -> None:
+        docs = self.rag.search(query, k)
+        print(docs[0])
+        res = StudentSearchResults(
+            search_results=[
+                MinimalSearchResults(
+                    question_id="1",
+                    question=query,
+                    retrieved_sources=[
+                        MinimalSource(**doc) for doc in docs[0]
+                    ],
+                )
+            ],
+            k=k,
         )
-        docs, scores = ret_loaded.retrieve(bm25s.tokenize(query), k=k)
-        return docs
+        self.rag.save_model("data/output", "search_result.json", res)
 
     def search_dataset(
         self,
         dataset_path: str = "datasets_public/public/UnansweredQuestions/dataset_code_public.json",
         k: int = 1,
-        save_directory: str = "test",
+        save_directory: str = "data/output",
     ):
-        search_results: list[MinimalSearchResults] = []
-        with open(dataset_path, "r") as f:
-            rag_dataset = RagDataset.model_validate_json(f.read())
-        for question in rag_dataset.rag_questions:
-            search_res = self.search(question.question, k)
-            search_results.append(
-                MinimalSearchResults(
-                    question_id=question.question_id,
-                    question=question.question,
-                    retrieved_sources=[
-                        MinimalSource(**doc) for doc in search_res[0]
-                    ],
-                )
-            )
-        res = StudentSearchResults(search_results=search_results, k=k)
-        if not os.path.isdir(save_directory):
-            os.mkdir(save_directory)
-        with open(f"{save_directory}/output.json", "w") as f:
-            json.dump(json.loads(res.model_dump_json()), f)
+        self.rag.search_dataset(dataset_path, k, save_directory)
 
     def answer(self, prompt: str, k: int = 2):
-        unanswered = UnansweredQuestion(question=prompt)
-        docs = self.search(prompt, k)
-        context = "### RETRIEVED_CONTEXT ###\n"
-        for doc in docs[0]:
-            context += f"# SOURCE: {doc['file_path']}\n"
-            context += "---\n"
-            context += doc["page_content"]
-            context += "---\n"
-        tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
-        model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B")
-        messages = [
-            {"role": "system", "content": Answer().limit(context)},
-            {"role": "user", "content": "/no_think " + prompt},
-        ]
-        print(len(Answer().limit(prompt)))
-        inputs = tokenizer.apply_chat_template(
-            messages,
-            add_generation_prompt=True,
-            tokenize=True,
-            return_dict=True,
-            return_tensors="pt",
-        ).to(model.device)
-        start = time.time()
-        outputs = Answer().generate_answer(model, inputs)
-        print("Time:", time.time() - start)
-        answer = re.sub(
-            r"<think>.*?</think>",
-            "",
-            Answer().decode(tokenizer, inputs, outputs),
-            flags=re.DOTALL,
-        ).strip()
-        res = StudentSearchResultsAndAnswer(
-            search_results=[
-                MinimalAnswer(
-                    question_id=unanswered.question_id,
-                    question=prompt,
-                    retrieved_sources=[
-                        MinimalSource(**doc) for doc in docs[0]
-                    ],
-                    answer=answer,
-                )
-            ],
-            k=k,
-        )
-        res_json = json.loads(res.model_dump_json())
-        if not os.path.isdir("data/output"):
-            os.mkdir("data/output")
-        with open("data/output/search_result.json", "w") as f:
-            json.dump(res_json, f)
+        self.rag.answer(prompt, k)
 
     def answer_dataset(self):
         pass
---
+++ student/generate.py
@@ -0,0 +1,78 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from student.validator import (
+    MinimalAnswer,
+    MinimalSource,
+    StudentSearchResultsAndAnswer,
+    UnansweredQuestion,
+)
+
+
+class Generate:
+    def __init__(self, docs: list[MinimalSource], prompt: str, k: int) -> None:
+        self.docs: list[MinimalSource] = docs
+        self.context: None | str = None
+        self.tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
+        self.model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B")
+        self.prompt = prompt
+        self.k = k
+
+    def generate_context(self):
+        context = "### RETRIEVED_CONTEXT ###\n"
+        for doc in self.docs:
+            context += f"# SOURCE: {doc.file_path}\n"
+            context += "---\n"
+            context += doc.page_content
+            context += "---\n"
+        return context
+
+    def generate_inputs(self, context: str, prompt: str):
+        messages = [
+            {"role": "system", "content": self.limit(context)},
+            {"role": "user", "content": "/no_think " + prompt},
+        ]
+        inputs = self.tokenizer.apply_chat_template(
+            messages,
+            add_generation_prompt=True,
+            tokenize=True,
+            return_dict=True,
+            return_tensors="pt",
+        ).to(self.model.device)
+        return inputs
+
+    @torch.inference_mode()
+    def generate_answer(self, data) -> str:
+        return self.model.generate(
+            **data,
+            max_new_tokens=50,
+            do_sample=False,
+            use_cache=True,
+            cache_implementation="static",
+            # torch_dtype=torch.float16,
+            # device_map="auto",
+        )
+
+    def decode(self, inputs, outputs):
+        index = inputs["input_ids"].shape[-1]
+        return self.tokenizer.decode(outputs[0][index:])
+
+    def limit(self, string: str):
+        if len(string) > 400:
+            return string[:400]
+        return string
+
+    def generate_model(self, answer: str):
+        return StudentSearchResultsAndAnswer(
+            search_results=[
+                MinimalAnswer(
+                    question_id=UnansweredQuestion(
+                        question=self.prompt
+                    ).question_id,
+                    question=self.prompt,
+                    retrieved_sources=self.docs,
+                    answer=answer,
+                )
+            ],
+            k=self.k,
+        )
